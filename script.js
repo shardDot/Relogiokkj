1c0b825d74e032a04a9aa7dc03911fa3c7bf9df6
Shard <oshards@proton.me>
2025-12-14 16:40:22 -0300

    Toggle light/dark mode

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const latestStatus = document.querySelector("#latest-status")
-latestStatus.textContent = `Latest visit: ${localStorage.getItem("latestVisited")}`
-
+const latestStatus = document.querySelector("#latest-status");
+latestStatus.textContent = `Latest visit: ${localStorage.getItem(
+  "latestVisited"
+)}`;
 
 function atualizarRelogio() {
-    let agora = new Date()
+  let agora = new Date();
 
-    let horas = agora.getHours()
-    let minutos = agora.getMinutes()
-    let segundos = agora.getSeconds()
+  let horas = agora.getHours();
+  let minutos = agora.getMinutes();
+  let segundos = agora.getSeconds();
 
-    horas = horas < 10 ? `0${horas}:` : horas + ':'
-    minutos = minutos < 10 ? `0${minutos}:` : minutos + ':'
-    segundos = segundos < 10 ? "0" + segundos : segundos
+  horas = horas < 10 ? `0${horas}:` : horas + ":";
+  minutos = minutos < 10 ? `0${minutos}:` : minutos + ":";
+  segundos = segundos < 10 ? "0" + segundos : segundos;
 
-    const hoursEl = document.querySelector("#hours")
-    const minutesEl = document.querySelector("#minutes")
-    const secondsEl = document.querySelector("#seconds")
+  const hoursEl = document.querySelector("#hours");
+  const minutesEl = document.querySelector("#minutes");
+  const secondsEl = document.querySelector("#seconds");
 
-    hoursEl.textContent = horas
-    minutesEl.textContent = minutos
-    secondsEl.textContent = segundos
-    console.log(segundos)
-    latestVisited = `${horas}${minutos}${segundos}`
+  hoursEl.textContent = horas;
+  minutesEl.textContent = minutos;
+  secondsEl.textContent = segundos;
+  console.log(segundos);
+  latestVisited = `${horas}${minutos}${segundos}`;
 
-    localStorage.setItem("latestVisited", latestVisited)
+  localStorage.setItem("latestVisited", latestVisited);
 }
 
-setInterval(atualizarRelogio, 1000)
+setInterval(atualizarRelogio, 1000);
